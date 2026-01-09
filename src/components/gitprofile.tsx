a96b7ac114f562b3c6d5be33d9868c6a8cf98d1b
Meet1590 <meet3670patel@gmail.com>
2026-01-09 11:02:18 +0000

    Quick-fix: responsiveness adjusted for mobile devices

--- src/components/gitprofile.tsx
+++ src/components/gitprofile.tsx
@@ -230,7 +230,7 @@ const GitProfile = ({ config }: { config: Config }) => {
                 </div>
               </div>
               <div className="lg:col-span-2 col-span-1">
-                <div className="grid grid-cols-2 gap-6">
+                <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
                   {sanitizedConfig.projects.github.display && (
                     <GithubProjectCard
                       header={sanitizedConfig.projects.github.header}
